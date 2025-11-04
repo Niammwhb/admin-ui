be8192456fbc8aa269aacc41172621d27c980d7f
Niammwhb <niamabadi25@gmail.com>
2025-11-04 10:41:26 +0700

    Understanding JSX in Reactjs and ES6

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,35 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Service";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("[Component] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard
-            name="Muhammad"
-            email="muhammad@gmail.com"
-            street="Jl. Indrapasta"
-            city="Semarang"
-          />
-          <UserCard
-            name="Ni'am"
-            email="niam@gmail.com"
-            street="Jl. Pemuda"
-            city="Ungaran"
-          />
-          <UserCard
-            name="Mawahib"
-            email="mawahib@gmail.com"
-            street="Jl. Patebon"
-            city="Kendal"
-          />
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Cards
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {}
+        {users.map((user) => (
+          <UserCard key={user.email} {...user} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 function UserCard(props) {
-  const { name, email, street, city } = props;
+  const { name, email, street, city, ...rest } = props;
   const [clicked, setClicked] = useState(false);
+
+  console.log(Object.entries(rest));
 
   return (
     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
@@ -14,6 +16,14 @@ function UserCard(props) {
         <span className="font-medium">Address:</span>
         {street},{city}
       </p>
+
+      {/* Menampilkan data tambahan dari rest */}
+      {Object.entries(rest).map(([key, value]) => (
+        <p key={key} className="text-gray-600">
+          <span className="font-medium capitalize">{key}:</span> {value}
+        </p>
+      ))}
+
       <button
         className={`${
           clicked ? "bg-special-green" : "bg-gray-01"
